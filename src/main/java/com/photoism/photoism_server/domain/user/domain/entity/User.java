f8229dbeae4b3b800b438f729ea3c5218c1a7e21
ta2ye0n <tyeon42051@naver.com>
2024-10-30 05:01:56 +0900

    add :: User

---
+++ src/main/java/com/photoism/photoism_server/domain/user/domain/entity/User.java
@@ -0,0 +1,30 @@
+package com.photoism.photoism_server.domain.user.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String username; // 유저가 입력한 ID
+
+    private String password;
+
+    private String email;
+
+}
