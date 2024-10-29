c475087b93f724d2652e2b5f5f6209fa1b70a3a8
ta2ye0n <tyeon42051@naver.com>
2024-10-30 05:01:47 +0900

    add :: Shape

---
+++ src/main/java/com/photoism/photoism_server/domain/correction/domain/enums/Shape.java
@@ -0,0 +1,10 @@
+package com.photoism.photoism_server.domain.correction.domain.enums;
+
+public enum Shape {
+    // 얼굴형 - 데일리, 내츄럴, 베이비, 슬림, 다크
+    DAILY,
+    NATURAL,
+    BABY,
+    SLIM,
+    DARK
+}
