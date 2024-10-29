20f39fd4dbee062fe0c6e0b76c171d02b0a6599c
한태영 <s46422574@gmail.com>
2024-10-29 17:47:45 +0900

    feat :: 의존성 추가

---
+++ src/test/java/com/photoism/photoism_server/PhotoismServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photoism.photoism_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoismServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
