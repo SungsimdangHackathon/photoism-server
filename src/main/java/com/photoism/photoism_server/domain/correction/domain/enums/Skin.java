170288503480a0ba80e6a2801e3c63c8b2acf930
ta2ye0n <tyeon42051@naver.com>
2024-10-30 05:01:52 +0900

    add :: Skin

---
+++ src/main/java/com/photoism/photoism_server/domain/correction/domain/enums/Skin.java
@@ -0,0 +1,10 @@
+package com.photoism.photoism_server.domain.correction.domain.enums;
+
+public enum Skin {
+    // 피부 - 라이트, 핑크, 웜, 카멜, 다크
+    LIGHT,
+    PINK,
+    WARM,
+    CAMEL,
+    DARK
+}
